75b7a4bda0149ede90f618a8d0efc7f899ef6523
Dominik Pokorný <126150424+Dommss@users.noreply.github.com>
2023-10-05 16:08:33 +0200

    making the upgrade functional

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -1,6 +1,11 @@
 extends Node
 
 signal experience_crab_juice_collected(number: float)
+signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary)
 
 func emit_crab_juice_exp_collected(number: float):
 	experience_crab_juice_collected.emit(number)
+
+
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	ability_upgrade_added.emit(upgrade, current_upgrades)
